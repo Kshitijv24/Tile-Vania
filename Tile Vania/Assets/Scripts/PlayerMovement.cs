e5dc3002d2c8a0130f9f33043e92647b060b8fa1
Kshitijv24 <kshitijv24@gmail.com>
2022-07-24 12:12:21 +0530

    Changing player animation state from idle to running if our player is moving

--- Tile Vania/Assets/Scripts/PlayerMovement.cs
+++ Tile Vania/Assets/Scripts/PlayerMovement.cs
@@ -7,21 +7,37 @@ public class PlayerMovement : MonoBehaviour
 
     private Vector2 moveInput;
     private Rigidbody2D rb;
+    private Animator animator;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         Run();
+        FlipSprite();
     }
 
     private void Run()
     {
         Vector2 playerVelocity = new Vector2(moveInput.x * moveSpeed, rb.velocity.y);
         rb.velocity = playerVelocity;
+
+        bool playerIsRunning = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+        animator.SetBool("isRunning", playerIsRunning);
+    }
+
+    private void FlipSprite()
+    {
+        bool playerIsRunning = Mathf.Abs(rb.velocity.x) > Mathf.Epsilon;
+
+        if (playerIsRunning)
+        {
+            transform.localScale = new Vector2(Mathf.Sign(rb.velocity.x), 1f);
+        }
     }
 
     private void OnMove(InputValue value)
