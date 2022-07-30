cff31695464cea2bad816195899a1e7ab85c5e4b
Kshitijv24 <kshitijv24@gmail.com>
2022-07-30 10:18:34 +0530

    added some levels in the game also added level transition from one level to another

---
+++ Tile Vania/Assets/Scripts/LevelExit.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class LevelExit : MonoBehaviour
+{
+    [SerializeField] private float levelLoadDelay = 0.5f;
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        StartCoroutine(LoadNextLeve());
+    }
+
+    IEnumerator LoadNextLeve()
+    {
+        yield return new WaitForSecondsRealtime(levelLoadDelay);
+
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if(nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+
+        SceneManager.LoadScene(nextSceneIndex);
+
+        
+    }
+}
