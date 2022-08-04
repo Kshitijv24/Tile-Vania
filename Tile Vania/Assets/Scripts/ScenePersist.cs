005122b1c976bce027d46c2f9d9f28caf80b1363
Kshitijv24 <kshitijv24@gmail.com>
2022-08-04 11:48:11 +0530

    added Scene Persist in the game so that we can keep the record of the coins collected and enemy killed numbers, so if we collect a coin or kill a enemy so they won't reappear if we lost a life in a level

---
+++ Tile Vania/Assets/Scripts/ScenePersist.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+public class ScenePersist : MonoBehaviour
+{
+    private void Awake()
+    {
+        int numberOfScenePersist = FindObjectsOfType<ScenePersist>().Length;
+
+        if (numberOfScenePersist > 1)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            DontDestroyOnLoad(gameObject);
+        }
+    }
+
+    public void ResetScenePersist()
+    {
+        Destroy(gameObject);
+    }
+}
